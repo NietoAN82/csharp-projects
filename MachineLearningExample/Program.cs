ef0a2f59c62096dc8573a8edbd337c5548907f96
Alfredo Nieto <Nieto.Alfredo@outlook.com>
2023-05-12 10:33:23 -0500

    Added example of using Machine Learning to predict the price of a Computer based on RAM Size using  previous data provided.

--- MachineLearningExample/Program.cs
+++ MachineLearningExample/Program.cs
@@ -1,10 +1,48 @@
-﻿namespace MachineLearningExample
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using System.Diagnostics;
+
+
+namespace MachineLearningExample
 {
-    internal class Program
+    class Program
     {
+        public class ComputerData
+        {
+            public float RAM { get; set; }
+            public float Price { get; set; }
+        }
+
+        public class Prediction
+        {
+            [ColumnName("Score")]
+            public float Price { get; set; }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MLContext mlContext = new MLContext();
+
+            ComputerData[] computerData =
+            {
+                new ComputerData() {RAM = 8, Price=800},
+                new ComputerData() {RAM = 16,Price=1000},
+                new ComputerData() {RAM = 32, Price=1500},
+                new ComputerData() {RAM=64, Price=2500}
+            };
+
+            IDataView trainingdata = mlContext.Data.LoadFromEnumerable(computerData);
+
+            var pipeline = mlContext.Transforms.Concatenate("Features",
+                new[] { "RAM" }).Append(mlContext.Regression.Trainers.Sdca(labelColumnName: "Price", maximumNumberOfIterations: 100));
+
+            var model = pipeline.Fit(trainingdata);
+
+            ComputerData randomAccessMemory = new ComputerData() { RAM = 100 };
+
+            Prediction price = mlContext.Model.CreatePredictionEngine<ComputerData, Prediction>(model).Predict(randomAccessMemory);
+
+            Debug.WriteLine($"Predicted price for RAM: {randomAccessMemory.RAM} price = {price.Price:C}");
         }
     }
 }
